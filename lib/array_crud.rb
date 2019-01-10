7c64bd960b357336b4779f0f5e0ac3e1c3f0e347
JanCarlos Acevedo <jancarlos92@me.com>
2019-01-10 18:12:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   element = "v"
-  create_an_array.unshift(element)
+  create_an_array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
