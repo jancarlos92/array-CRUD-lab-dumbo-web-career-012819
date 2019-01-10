aafcdd9a703c57f1ed9d8815c723406a78a12e35
JanCarlos Acevedo <jancarlos92@me.com>
2019-01-10 18:16:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  element = "arrays!"
+  create_an_arrayindex_number
   create_an_array.push(element)
 end
 
