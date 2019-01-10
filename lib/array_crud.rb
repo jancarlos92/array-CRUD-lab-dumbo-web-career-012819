7410d20da57dfdcad9a25ba39ca25e35a0f42296
JanCarlos Acevedo <jancarlos92@me.com>
2019-01-10 18:18:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,16 +27,14 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  create_an_array(index_number
-  create_an_array.push(element)
-end
+  index_number >= 0
+  create_an_array[index_number]
+  end
 
 def retrieve_first_element_from_array(array)
-  element = "arrays!"
-  create_an_array.push(element)
+  create_an_array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  element = "arrays!"
-  create_an_array.push(element)
+  create_an_array[-1]
 end
